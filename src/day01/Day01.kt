f0c06a152d0d276beb9a63f8ad5524142cf9b295
djleeds <dave@typealias.com>
2024-12-01 21:09:20 -0600

    Solve Day 1

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,17 +1,43 @@
 package day01
 
+import solve
+import kotlin.math.absoluteValue
+
 fun main() {
+    fun parse(input: List<String>): Pair<List<Int>, List<Int>> {
+        val pairs = input.map {
+            val (a, b) = it.split("   ")
+            a.toInt() to b.toInt()
+        }
+
+        val list1 = pairs.map { it.first }.sorted()
+        val list2 = pairs.map { it.second }.sorted()
+
+        return list1 to list2
+    }
+
     fun part1(input: List<String>): Int {
-        return 0
+        val (list1, list2) = parse(input)
+        check(list1.size == list2.size)
+
+        val result = list1.zip(list2) { left, right -> (left - right).absoluteValue }.sum()
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val (list1, list2) = parse(input)
+
+        val result = list1.sumOf { left ->
+            val appearances = list2.count { it == left }
+            left * appearances
+        }
+
+        return result
     }
 
-//    solve(::part1, withInput = "day01/test1", andAssert = 142)
-//    solve(::part1, withInput = "day01/input", andAssert = 55834)
-//
-//    solve(::part2, withInput = "day01/test2", andAssert = 281)
-//    solve(::part2, withInput = "day01/input", andAssert = 53221)
+    solve(::part1, withInput = "day01/test", andAssert = 11)
+    solve(::part1, withInput = "day01/input", andAssert = 1765812)
+
+    solve(::part2, withInput = "day01/test", andAssert = 31)
+    solve(::part2, withInput = "day01/input", andAssert = 20520794)
 }
